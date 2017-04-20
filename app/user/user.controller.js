f8eb5557b449ca473a4fdeec013ddc817da1644e
Daniel Canqui <you.nightmare.00@gmail.com>
2017-04-19 21:25:39 -0400

    Removed some console logs.

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -1,12 +1,11 @@
 const User = require('./user.model.js');
 
 function createUser(req, res) {
-  console.log(req.body.userData);
   User.create(req.body.userData)
     .then(function(result) {
       res.status(201).send('User is created.');
     }).catch(function(error) {
-      console.log(error);
+      res.status(500).send('User is not created');
     });
 }
  module.exports = {
